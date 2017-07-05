e10cc940a599ae54a8b6dce3b0d908f63a3095de
arjun289 <arjun289singh@gmail.com>
2017-07-05 02:13:47 +0000

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+    validates :title, uniqueness: true
+    validates :title, :image_url, :description, presence: true
+    validates :price, numericality: {greater_than_or_equal_to: 0.01}
+    validates :image_url, allow_blank: true, format: {
+        with:  %r{\.(gif|jpg|png)\Z}i,
+        message: 'must be a gif, jpg or png file'
+    }
 end
