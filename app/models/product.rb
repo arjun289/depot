71b490132639f7afcb2b5557432ed8b61ddb2ad4
arjun289 <arjun289singh@gmail.com>
2017-07-06 09:03:00 +0000

    cart creation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,21 @@
 class Product < ApplicationRecord
-    validates :title, uniqueness: true
-    validates :title, :image_url, :description, presence: true
-    validates :price, numericality: {greater_than_or_equal_to: 0.01}
-    validates :image_url, allow_blank: true, format: {
-        with:  %r{\.(gif|jpg|png)\Z}i,
-        message: 'must be a gif, jpg or png file'
-    }
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  validates :title, uniqueness: true
+  validates :title, :image_url, :description, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :image_url, allow_blank: true, format: {
+      with:  %r{\.(gif|jpg|png)\Z}i,
+      message: 'must be a gif, jpg or png file'
+  }
+
+  private
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items Present')
+        throw :abort
+      end
+    end
+    
 end
